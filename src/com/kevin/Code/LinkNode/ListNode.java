433da2c80e7508035e5d2d608a034d887d543b9f
KevinJavaLee <1401597760@qq.com>
2021-03-26 20:11:14 +0800

    ListNode

---
+++ src/com/kevin/Code/LinkNode/ListNode.java
@@ -0,0 +1,15 @@
+package com.kevin.Code.LinkNode;
+
+/**
+ * @author Vinlee xiao
+ * @Description: TODO
+ * @create 2021-03-26 16:50
+ * @project LeetCode
+ * @Version 1.0
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+}
