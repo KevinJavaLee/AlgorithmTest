e0db483958c56973a18d9ff8d81b84299b7ddb5c
KevinJavaLee <1401597760@qq.com>
2021-04-09 22:27:31 +0800

    Node

---
+++ src/com/kevin/Code/LinkNode/Node.java
@@ -0,0 +1,20 @@
+package com.kevin.Code.LinkNode;
+
+/**
+ * @author Vinlee xiao
+ * @Description: TODO
+ * @create 2021-04-09 22:26
+ * @project LeetCode
+ * @Version 1.0
+ */
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
