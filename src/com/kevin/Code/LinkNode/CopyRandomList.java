8ae8982ae7a15a35a2314a9421e661a5ba84e0b4
KevinJavaLee <1401597760@qq.com>
2021-04-19 17:26:03 +0800

    测试

---
+++ src/com/kevin/Code/LinkNode/CopyRandomList.java
@@ -0,0 +1,24 @@
+package com.kevin.Code.LinkNode;
+
+/**
+ * @author Vinlee xiao
+ * @Description: 剑指 Offer 35. 复杂链表的复制
+ * @create 2021-04-09 22:25
+ * @project LeetCode
+ * @Version 1.0
+ */
+public class CopyRandomList {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 复杂链表的复制
+     * @param head
+     * @return
+     */
+    public static Node copyRandomList(Node head) {
+
+        return null;
+    }
+}
