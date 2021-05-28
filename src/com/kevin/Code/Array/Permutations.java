47b0fd615b59ff1c5d96fac1798d198ce5a7e962
KevinJavaLee <1401597760@qq.com>
2021-05-28 15:08:01 +0800

    Leetcode 46 全排列 中等难度 回溯排序

---
+++ src/com/kevin/Code/Array/Permutations.java
@@ -0,0 +1,27 @@
+package com.kevin.Code.Array;
+
+import java.util.List;
+
+/**
+ * @Project LeetCode
+ * @ClassName Permutations
+ * @Description Leetcode 46 全排列 中等难度 回溯排序
+ * @Author Vinlee Xiao
+ * @Date 2021/05/19/22:05
+ * @Version 1.0
+ */
+public class Permutations {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> permute(int[] nums) {
+
+        return null;
+    }
+}
