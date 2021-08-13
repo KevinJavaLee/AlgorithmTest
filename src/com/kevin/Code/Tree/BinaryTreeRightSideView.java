20e1a8b55644a02918f4a58eb2f84fa5425c502c
KevinJavaLee <1401597760@qq.com>
2021-08-13 21:20:27 +0800

    Leetcode 151. 翻转字符串里的单词 同剑指 Offer 58 - I. 翻转单词顺序 中等难度

---
+++ src/com/kevin/Code/Tree/BinaryTreeRightSideView.java
@@ -0,0 +1,30 @@
+package com.kevin.Code.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Vinlee Xiao
+ * @Classname BinaryTreeRightSideView
+ * @Description Leetcode 199. 二叉树的右视图 中等难度 没啥思路 高频
+ * @Date 2021/8/11 16:44
+ * @Version 1.0
+ */
+public class BinaryTreeRightSideView {
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * @param root
+     * @return
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        return null;
+    }
+}
